a3ce95ca7d8a4cd63387c0ebc7bfd9bb51a604ac
Michael Teichgräber <mteichgraeber@gmx.de>
2017-10-30 01:01:11 +0100

    document vfsutil.go

--- vfsutil.go
+++ vfsutil.go
@@ -1,3 +1,9 @@
+// Copyright 2015 M. Teichgräber. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package vfsutil implements some helper types and functions for
+// use with godoc's vfs package.
 package vfsutil
 
 import (
@@ -18,18 +24,23 @@ func (f *file) Label() string {
 	return f.label
 }
 
+// The Label interface is satisfied by the `file` object returned
+// by labeledFileSystem.Open satisfies this interface.
 type Label interface {
 	Label() string
 }
 
+// LabeledFS attaches a label to an existing vfs.FileSystem.
 func LabeledFS(fs vfs.FileSystem, label string) vfs.FileSystem {
 	return &labeledFileSystem{FileSystem: fs, label: label}
 }
 
+// LabeledOS calls vfs.OS and attaches a label to the vfs.FileSystem created.
 func LabeledOS(root string, label string) vfs.FileSystem {
 	return &labeledFileSystem{FileSystem: vfs.OS(root), label: label}
 }
 
+// Open implements a vfs.Opener for labeledFileSystem
 func (fs *labeledFileSystem) Open(name string) (vfs.ReadSeekCloser, error) {
 	rsc, err := fs.FileSystem.Open(name)
 	if err != nil {
