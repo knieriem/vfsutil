01117abe688d60f0a2cb16a3fa78703823874951
Michael Teichgräber <mteichgraeber@gmx.de>
2019-05-10 13:02:50 +0200

    rename Label interface into FSInfo, add Root() method Root() returns the path to the root of the file system, if created using LabeledOS(). If fsInfo.root is empty, Root() returns a value derived from fsInfo.label.

--- vfsutil.go
+++ vfsutil.go
@@ -14,32 +14,51 @@ import (
 
 type labeledFileSystem struct {
 	vfs.FileSystem
-	label string
+	fi *fsInfo
 }
 
 type file struct {
 	vfs.ReadSeekCloser
+	*fsInfo
+}
+
+type fileInfo struct {
+	os.FileInfo
+	*fsInfo
+}
+
+type fsInfo struct {
 	label string
+	root  string
 }
 
-func (f *file) Label() string {
-	return f.label
+func (fi *fsInfo) Label() string {
+	return fi.label
 }
 
-// The Label interface is satisfied by the file object returned
-// by labeledFileSystem.Open.
-type Label interface {
+func (fi *fsInfo) Root() string {
+	if fi.root != "" {
+		return fi.root
+	}
+	return "[" + fi.label + "]:"
+}
+
+// The FSInfo interface is satisfied by the file object returned
+// by labeledFileSystem.Open, and by the fileInfo object
+// returned by labeledFileSystem.Stat.
+type FSInfo interface {
 	Label() string
+	Root() string
 }
 
 // LabeledFS attaches a label to an existing vfs.FileSystem.
 func LabeledFS(fs vfs.FileSystem, label string) vfs.FileSystem {
-	return &labeledFileSystem{FileSystem: fs, label: label}
+	return &labeledFileSystem{FileSystem: fs, fi: &fsInfo{label: label}}
 }
 
 // LabeledOS calls vfs.OS and attaches a label to the vfs.FileSystem created.
 func LabeledOS(root string, label string) vfs.FileSystem {
-	return &labeledFileSystem{FileSystem: vfs.OS(root), label: label}
+	return &labeledFileSystem{FileSystem: vfs.OS(root), fi: &fsInfo{label: label, root: root}}
 }
 
 // Open implements a vfs.Opener for labeledFileSystem
@@ -48,7 +67,7 @@ func (fs *labeledFileSystem) Open(name string) (vfs.ReadSeekCloser, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &file{ReadSeekCloser: rsc, label: fs.label}, nil
+	return &file{ReadSeekCloser: rsc, fsInfo: fs.fi}, nil
 }
 
 // Stat implements vfs.FileSystem.Stat for labeledFileSystem
@@ -57,14 +76,5 @@ func (fs *labeledFileSystem) Stat(name string) (os.FileInfo, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &fileInfo{FileInfo: fi, label: fs.label}, nil
-}
-
-type fileInfo struct {
-	os.FileInfo
-	label string
-}
-
-func (fi *fileInfo) Label() string {
-	return fi.label
+	return &fileInfo{FileInfo: fi, fsInfo: fs.fi}, nil
 }
